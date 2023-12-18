65f9aad39fb4894977885ce797ed068cdef16640
Carlos A Palacios R <carlosargelio0104@gmail.com>
2023-12-18 18:18:10 -0400

    feat: intento #01

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,66 @@
-import React, { useState } from "react"
-import "./App.css"
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./App.css";
 
-const initialTask = [
+const initialTasks = [
   {
     id: "1",
-    text: "JavaScript"
+    text: "React.js",
   },
   {
     id: "2",
-    text: "Python"
+    text: "HTML/CSS",
   },
   {
-    id: "2",
-    text: "Rush"
+    id: "3",
+    text: "AWS",
   },
   {
-    id: "2",
-    text: "Older"
-  }
-]
+    id: "4",
+    text: "JavaScript",
+  },
+];
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
 
-  const [task, setTask] = useState(initialTask);
+  const onDragEnd = (result) => {
+    console.log(result);
+    // Implement your logic for updating the order of tasks here
+  };
 
   return (
-    <React.Fragment>
-      <h1>Estudiar</h1>
-      <ul className="task-container">
-        {task.map((task) => (
-          <li className="task-item" key={task.id}>{task.text}</li>
-        ))} 
-      </ul>
-    </React.Fragment>
-  )
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="app">
+        <h1>Estudiar</h1>
+        <Droppable droppableId="tasks" direction="vertical">
+          {(provided) => (
+            <ul
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="task-container"
+            >
+              {tasks.map((task, index) => (
+                <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                  {(provided) => (
+                    <div
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                      className="task-item"
+                    >
+                      {task.text}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
-export default App
+export default App;
