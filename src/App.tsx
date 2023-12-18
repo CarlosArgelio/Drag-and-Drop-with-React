124491e31ba7c96cc968d377e23aa01687722261
Carlos A Palacios R <carlosargelio0104@gmail.com>
2023-12-18 17:08:38 -0400

    style: add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
+import "./App.css"
+
+const initialTask = [
+  {
+    id: "1",
+    text: "JavaScript"
+  },
+  {
+    id: "2",
+    text: "Python"
+  },
+  {
+    id: "2",
+    text: "Rush"
+  },
+  {
+    id: "2",
+    text: "Older"
+  }
+]
 
 function App() {
 
+  const [task, setTask] = useState(initialTask);
+
   return (
     <React.Fragment>
-      <p>Hello World</p>
+      <h1>Estudiar</h1>
+      <ul className="task-container">
+        {task.map((task) => (
+          <li className="task-item" key={task.id}>{task.text}</li>
+        ))} 
+      </ul>
     </React.Fragment>
   )
 }
